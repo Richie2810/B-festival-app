c852c71709ac40968d5ad02995172bd2190ea2b1
Richie2810 <richiegoneill@gmail.com>
2021-04-05 23:13:36 +0200

    adding a route to stages, that gets all acts of a certain day. for all stages

--- models/stage.js
+++ models/stage.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       type:DataTypes.STRING,
       allowNull: false 
     },
-    isVip:  {
+    isVIP:  {
       type:DataTypes.BOOLEAN,
       defaultValue: false 
     }
---
+++ routers/stages.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const Stage = require('../models').stage
+const Act = require('../models').act
+
+const router = new Router();
+
+router.post('/', async (req,res,next) => {
+    try {
+        const { day } = req.body
+        console.log(day)
+        const actPerDay = await Act.findAll({
+            where: {day:day},
+            include : [Stage],
+        })
+
+        return res.status(200).send(actPerDay)
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({ message: "Something went wrong, sorry" });
+    }
+})
+
+module.exports = router;
