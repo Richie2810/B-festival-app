18092d32e19eee2667ff6e326bc3753696467606
Richie2810 <richiegoneill@gmail.com>
2021-04-09 21:58:14 +0200

    added route to add plans to DB

--- routers/plans.js
+++ routers/plans.js
@@ -8,13 +8,27 @@ const User = require('../models').user
 const router = new Router();
 
 router.get('/', authMiddleware, async (req,res,next)=>{
-    console.log('This is the users id', req.user.id)
     try{
         const user = await User.findByPk(req.user.id,
             {
                 include: Act
             })
         res.status(200).send(user.acts)
+    } catch(e){
+        console.log(e.message)
+        return res.status(400).send("Date is wrong")
+    }
+})
+
+router.post('/addPlan',authMiddleware, async (req,res,next) => {
+    const { actId } = req.body
+    console.log(actId)
+    try {
+        const newPlan = await Plan.create({
+            userId: req.user.id,
+            actId
+        })
+        return res.status(200).send(newPlan)
     } catch(e){
         console.log(e.message)
         return res.status(400).send("Date is wrong")
